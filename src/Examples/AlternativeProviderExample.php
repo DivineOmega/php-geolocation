96a71754a24cb14f8846e1a6814beb008031fbb2
Milo Cesar <milocesar1@gmail.com>
2018-10-01 22:46:24 +0200

    Switched free geopip to ipstack

--- src/Examples/AlternativeProviderExample.php
+++ src/Examples/AlternativeProviderExample.php
@@ -3,10 +3,10 @@
 require_once __DIR__.'/../../vendor/autoload.php';
 
 use DivineOmega\Geolocation\Locator;
-use DivineOmega\Geolocation\LocationProviders\FreeGeoIP;
+use DivineOmega\Geolocation\LocationProviders\IpStack;
 
 $locator = new Locator;
-$locator->setLocationProvider(new FreeGeoIP);
+$locator->setLocationProvider(new IpStack("my_ip_stack_api_key"));
 
 $country = $locator->getCountryByIP('81.150.13.20');
 
--- src/LocationProviders/IpStack.php
+++ src/LocationProviders/IpStack.php
@@ -6,19 +6,23 @@ use DivineOmega\Geolocation\Interfaces\LocationProviderInterface;
 use DivineOmega\Countries\Countries;
 use GuzzleHttp\Client;
 
-class FreeGeoIP implements LocationProviderInterface
+class IpStack implements LocationProviderInterface
 {
     private $client;
+    private $api_key;
 
-    public function __construct($jsonBaseUri = 'https://freegeoip.net/json/')
+    public function __construct($api_key, $jsonBaseUri = 'http://api.ipstack.com/')
     {
+        $this->api_key = $api_key;
         $this->client = new Client(['base_uri' => $jsonBaseUri]);
     }
 
     public function getCountryByIP(string $ip)
     {
-        $response = (string) $this->client->get(urlencode($ip))->getBody();
-        
+        $arguments = '?access_key=' . $this->api_key . '&output=json';
+
+        $response = (string) $this->client->get(urlencode($ip) . $arguments)->getBody();
+
         if (!$response) {
             return null;
         }
